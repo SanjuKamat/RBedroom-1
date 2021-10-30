3ba8dfa609cfb47f8c362f1bb54cd1cc3fd72627
Sanju Kamat <sanjukamat123@gmail.com>
2021-10-30 11:53:43 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,195 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20211027_110835_00_merged",
+      "name": "IMG_20211027_110835_00_merged",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03648592820724694,
+          "pitch": 0.11848297124013385,
+          "rotation": 29.845130209103058,
+          "target": "1-img_20211027_110914_00_merged"
+        },
+        {
+          "yaw": -1.479572118577762,
+          "pitch": 0.08871180518338306,
+          "rotation": 7.853981633974483,
+          "target": "2-img_20211027_111035_00_merged"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1491467507276365,
+          "pitch": -0.22587010805455776,
+          "title": "Standard Room",
+          "text": "Price 1300-1600"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20211027_110914_00_merged",
+      "name": "IMG_20211027_110914_00_merged",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9377435236599823,
+          "pitch": 0.051192436426191534,
+          "rotation": 12.566370614359176,
+          "target": "0-img_20211027_110835_00_merged"
+        },
+        {
+          "yaw": -2.5457092095538005,
+          "pitch": 0.029359524199538356,
+          "rotation": 1.5707963267948966,
+          "target": "2-img_20211027_111035_00_merged"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9283030280482194,
+          "pitch": 0.26828788020110217,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-img_20211027_111035_00_merged",
+      "name": "IMG_20211027_111035_00_merged",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1301177574381036,
+          "pitch": 0.08855475962953818,
+          "rotation": 7.853981633974483,
+          "target": "0-img_20211027_110835_00_merged"
+        },
+        {
+          "yaw": 1.4565857257211938,
+          "pitch": 0.19424246948597457,
+          "rotation": 18.84955592153877,
+          "target": "3-img_20211027_111323_00_merged"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-img_20211027_111323_00_merged",
+      "name": "IMG_20211027_111323_00_merged",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.106435111079617,
+          "pitch": 0.1800650468741356,
+          "rotation": 4.71238898038469,
+          "target": "2-img_20211027_111035_00_merged"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
